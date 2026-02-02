b7b2f398681aadae57718c103e170ade17ad007b
omprakashpa <omprakashpa@cybage.com>
2026-02-02 12:20:11 +0530

    Create app.py

---
+++ app/app.py
@@ -0,0 +1,17 @@
+from flask import Flask, request
+import os
+
+app = Flask(__name__)
+
+@app.route("/")
+def home():
+    return "DevSecOps Lab"
+
+@app.route("/login")
+def login():
+    user = request.args.get("user")
+    query = "SELECT * FROM users WHERE username = '%s'" % user
+    return query
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5000)
