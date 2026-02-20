89c3ebde79a51c189859a6309af0301c0594a853
omprakashpa <omprakashpa@cybage.com>
2026-02-20 15:25:30 +0530

    Update app.py

--- app/app.py
+++ app/app.py
@@ -1,7 +1,7 @@
 AWS_ACCESS_KEY_ID = "AKIA1234567890TEST"
 AWS_ACCESS_KEY_ID = "AKIA1234567890TESTCHANGED"
 from flask import Flask, request
-import os
+import os 
 AWS_SECRET_ACCESS_KEY = "AKIA_ATTACKER_SIMULATION_KEY"
 app = Flask(__name__)
 
